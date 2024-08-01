84efe770bb651cd3392af4d00b0e9acae2d9c8d6
Catalina Tanasescu <catalinacatalina32@yahoo.com>
2024-08-01 15:27:48 +0300

    first commit

---
+++ Core/Inc/BMP180.h
@@ -0,0 +1,23 @@
+/*
+ * BMP180.h
+ *
+ *  Created on: Jul 30, 2024
+ *      Author: Marmelada
+ */
+
+#ifndef INC_BMP180_H_
+#define INC_BMP180_H_
+
+
+
+#include "stm32h5xx_hal.h"
+
+void BMP180_Start(void);
+
+float BMP180_GetTemp(void);
+
+float BMP180_GetPress(int oss);
+
+float BMP180_GetAlt(int oss);
+
+#endif /* INC_BMP180_H_ */
